ed6c7cd478bb7fcb4d76a6b88dea766f7864060c
acskata <93493050+acskata@users.noreply.github.com>
2022-10-30 16:38:19 +0000

    Rendered the word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,21 @@
 import React, { useState }from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [result, setResult] = useState(null);
 
     function handleResponse(response) {
-        console.log(response.data[0]);
+        setResult(response.data[0]);
     }
 
     function search(event) {
-        event.preventDeafult();
+        event.preventDefault();
 
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
-        return <p>Loading...</p>
     }
 
     function handleKeywordChange(event) {
@@ -27,33 +28,7 @@ export default function Dictionary() {
             <form onSubmit={search}>
                 <input type="search" onChange={handleKeywordChange} placeholder="Search for a word" autoFocus="on"/>
             </form>
-            <h2>Cloud [kloud]</h2>
-                <h3>noun</h3>
-                    <ol>
-                        <li>result 1</li>
-                        <li>result 2</li>
-                    </ol>
-                <h3>adjective</h3>
-                    <ol>
-                        <li>result 1</li>
-                        <li>result 2</li>
-                    </ol>
-                <h3>verb</h3>
-                    <ol>
-                        <li>result 1</li>
-                    </ol>
-                <p>Similar: xy, xy, xy</p>
-                <div className="row">
-                    <div className="col-4">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFcNLvwqAhkhkdDOg0FBDQ8foSq-8PK3sPAA&usqp=CAU" className="img-fluid rounded-2" alt="result"/>
-                    </div>
-                    <div className="col-4">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFcNLvwqAhkhkdDOg0FBDQ8foSq-8PK3sPAA&usqp=CAU" className="img-fluid rounded-2" alt="result"/>
-                    </div>
-                    <div className="col-4">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFcNLvwqAhkhkdDOg0FBDQ8foSq-8PK3sPAA&usqp=CAU" className="img-fluid rounded-2" alt="result"/>
-                    </div>
-                </div>
+            <Results result={result} />
         </div>
     );
 }
---
+++ src/Results.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Meaning from "./Meaning";
+
+export default function Results(props) {
+    if(props.result) {
+        return (
+            <div className="Results">
+                 <h2 className="text-capitalize">{props.result.word}</h2>
+                 <p>{props.result.phonetic}</p>
+                 {props.result.meanings.map(function(meaning, index){
+                    return (
+                    <div key={index}>
+                        <Meaning meaning={meaning} />
+                    </div>
+                    );
+                 })}
+            </div>
+        );
+    } else {
+        return null;
+    }
+}
