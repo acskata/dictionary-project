b162ace591c8b63e0c97143d8ad010050fffc455
acskata <93493050+acskata@users.noreply.github.com>
2022-11-06 15:36:29 +0000

    Added the new photos component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,32 @@
 import React, { useState }from 'react';
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
+
 
     function handleResponse(response) {
         setResults(response.data[0]);
     }
 
+    function handlePexelsResponse(response) {
+        setPhotos(response.data.photos);
+    }
+
     function search() {
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
+
+        let pexelsApiKey = "563492ad6f91700001000001fbde036c3992404e8ffc6c6e7aecb709";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+        axios.get(pexelsApiUrl, { headers: {"Authorization" : `Bearer ${pexelsApiKey}`}})
+        .then(handlePexelsResponse);
     }
 
     function handleSubmit(event) {
@@ -40,6 +52,7 @@ export default function Dictionary(props) {
                     </form>
                     </section>
                 <Results results={results} />
+                <Photos  photos={photos} />
             </div>
         );
     } else {
